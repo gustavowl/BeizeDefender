2ccb3e7222172c95a9089223fcdf57f259118202
Koruja <gustavoalvesbezerra@gmail.com>
2014-11-12 12:08:14 -0300

    Add lista.h

---
+++ projeto-edb-lp/lista.h
@@ -0,0 +1,219 @@
+#ifndef _LISTA_H_
+#define _LISTA_H_
+	#include <iostream>
+	#include <cstddef>
+
+	enum BEHAVIOR {LISTASIMPLES, PILHA, FILA}; //0, 1, 2
+
+	template <class type>
+	class Lista {
+	private:
+		int tam = 0;
+		BEHAVIOR bhvr;
+
+	public:
+		struct node{
+			type val;
+			node* next = NULL;
+		};
+		node* head = new node; //evita erro de apontar para 0x0 ou 0x1
+	//MÉTODOS
+		//construtores
+		Lista() {
+			bhvr = LISTASIMPLES;
+			head->next = NULL;
+		}
+
+		Lista(BEHAVIOR b) {
+			bhvr = b;
+			head->next = NULL;
+		}
+		//destructor
+		~Lista() {
+			if (bhvr == LISTASIMPLES)
+				while (Remove(0));
+			else
+				while (Remove());
+			//delete head;
+		}
+		//todos os métodos de com "int pos" funcionam como vetor (elementos de 0 até tamanho - 1)
+		int Size() {return tam;};
+
+		bool GetElem(int pos, type &get) { //retorna se posição é válida. &get armazenará valor
+			if (bhvr == LISTASIMPLES && pos >= 0 && pos < tam) {
+				node *temp = head;
+				for (int i = 0; i <= pos; i++) {
+					temp = temp->next;
+				}
+				get = temp->val;
+				return true;
+			}
+			return false;
+		};
+
+		bool GetElem(type &get) { //retorna se lista ou pilha possui elemento top. &get armazenará valor
+			if (tam > 0) {
+				if (bhvr == PILHA) {
+					get = head->next->val;
+					return true;
+				}
+				else if (bhvr == FILA) {
+					node* temp = head->next;
+					while (temp->next != NULL) {
+						temp = temp->next;
+					}
+					get = temp->val;
+					return true;
+
+				}
+				return false;
+			}
+			return false;
+		}
+
+		int Search(type val) { //retorna posição da primeira instância de val
+			//-1: não existe elemento
+			if (tam > 0) {
+				node *temp = head->next;
+				int i = 0;
+				do {
+					if (temp->val == val)
+						return i;
+					i++;
+					temp = temp->next;
+				} while (temp->next != NULL);
+				return -1;
+			}
+			return -1;
+		};
+		
+		bool Insert(int pos, type val) { //retorna se inserção OK. Insere val em lista simples
+			if (pos >= 0 && pos <= tam && bhvr == LISTASIMPLES) {
+				node *temp = head;
+				for (int i = 0; i < pos; i++) {
+					temp = temp->next;
+				}
+				node *novo = new node;
+				novo->val = val;
+				novo->next = temp->next;
+				temp->next = novo;
+				tam++;
+				return true;
+			}
+			return false;
+		}
+
+		bool Insert(type val) { //retorna se inserção OK. Insere val em filas e pilhas
+			if (tam >= 0 && (bhvr == PILHA || bhvr == FILA)) {
+				node *novo = new node;
+				while (novo == head) { //I HAVE NO IDEA OF WHY THIS HAPPENS
+					novo = new node;
+				}
+				novo->val = val;
+				novo->next = head->next;
+				head->next = novo;
+				tam++;
+				return true;
+			}
+			return false;
+		}
+
+		bool Remove(unsigned int pos) { //retorna se remoção OK. Não retorna valor. Funciona para listas
+			if (pos >= 0 && pos < tam && bhvr == LISTASIMPLES) {
+				node* ant = head;
+				node* temp = head->next;
+				for (int i = 0; i < pos; i++) {
+					temp = temp->next;
+					ant = ant->next;
+				}
+				ant->next = temp->next;
+				delete temp;
+				tam--;
+				return true;
+			}
+			return false;
+		};
+
+		bool Remove() {
+			if (tam > 0 && head->next != NULL) {
+				if (bhvr == PILHA) {
+					node *temp = head->next;
+					head->next = temp->next;
+					delete temp;
+					tam--;
+					return true;
+				}
+				else if (bhvr == FILA) {
+					node *bef = head; //before
+					node *fow = head->next; //foward
+					while (fow->next != NULL) {
+						fow = fow->next;
+						bef = bef->next;
+					}
+					delete fow;
+					bef->next = NULL;
+					tam--;
+					return true;
+				}
+				return false;
+			}
+			return false;
+		}
+
+		bool Remove(int pos, type &get) { //retorna se remoção OK. &get armazenará valor da posição
+			if (pos >= 0 && pos < tam && bhvr == LISTASIMPLES) {
+				node* ant = head;
+				node* temp = head->next;
+				for (int i = 0; i < pos; i++) {
+					temp = temp->next;
+					ant = ant->next;
+				}
+				ant->next = temp->next;
+				get = temp->val;
+				delete temp;
+				tam--;
+				return true;
+			}
+			return false;
+		};
+
+		bool Remove(type &get) { //se usar referência e type = int vai dar conflito com Remove(int pos)
+		//salvar imediatamente conteúdo de get em outra variável caso seja ponteiro
+			if (tam > 0) {
+				if (bhvr == PILHA) {
+					node *temp = head->next;
+					head->next = temp->next;
+					get = temp->val;
+					delete temp;
+					tam--;
+					return true;
+				}
+				else if (bhvr == FILA) {
+					node *bef = head; //before
+					node *fow = head->next; //foward
+					while (fow->next != NULL) {
+						fow = fow->next;
+						bef = bef->next;
+					}
+					get = fow->val;
+					delete fow;
+					bef->next = NULL;
+					tam--;
+					return true;
+				}
+				return false;
+			}
+			return false;
+		}
+
+		void Print(char separator = ' ') {
+			node *temp = head->next;
+			std::cout << std::endl;
+			while (temp != NULL) {
+				std::cout << temp->val << separator;
+				temp = temp->next;
+			}
+			std::cout << std::endl;
+		}
+	};
+#endif
