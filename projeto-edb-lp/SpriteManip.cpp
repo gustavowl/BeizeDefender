63e965f044d387d8eec2578233548416cfcd49e9
Koruja <gustavoalvesbezerra@gmail.com>
2014-11-21 18:44:42 -0300

    Adiciona Manipulador de sprites

---
+++ projeto-edb-lp/SpriteManip.cpp
@@ -0,0 +1,211 @@
+#include "SpriteManip.h"
+#include <math.h>
+#include <iostream>
+
+using namespace SpManip;
+
+SpriteManip::SpriteManip() {
+	DirecaoDestino = BAIXO;
+	DirecaoAlvo = BAIXO;
+	AcaoAtual = PARADO;
+	for ( int i = 0; i < QTD_DIRECOES; i++ ) {
+		Parado[i] = FilaDupl<ALLEGRO_BITMAP*>(); //cria fulas duplas vazias
+		Andar[i] = FilaDupl<ALLEGRO_BITMAP*>(); //cria fulas duplas vazias
+		Atirar[i] = FilaDupl<ALLEGRO_BITMAP*>(); //cria fulas duplas vazias
+		Morrer[i] = FilaDupl<ALLEGRO_BITMAP*>(); //cria fulas duplas vazias
+	}
+
+}
+
+SpriteManip::SpriteManip( FilaDupl<ALLEGRO_BITMAP*> parado[QTD_DIRECOES], 
+	FilaDupl<ALLEGRO_BITMAP*> andar[QTD_DIRECOES], 
+	FilaDupl<ALLEGRO_BITMAP*> atirar[QTD_DIRECOES], 
+	FilaDupl<ALLEGRO_BITMAP*> morrer[QTD_DIRECOES] ) {
+
+	DirecaoDestino = BAIXO;
+	DirecaoAlvo = BAIXO;
+	AcaoAtual = PARADO;
+
+	for ( int i = 0; i < QTD_DIRECOES; i++ ) {
+		Parado[i] = parado[i]; //cria fulas duplas vazias
+		Andar[i] = andar[i]; //cria fulas duplas vazias
+		Atirar[i] = atirar[i]; //cria fulas duplas vazias
+		Morrer[i] = morrer[i]; //cria fulas duplas vazias
+	}
+}
+
+SpriteManip::SpriteManip( int x_atual, int y_atual, int x_destino, int y_destino,
+	int x_alvo, int y_alvo,	FilaDupl<ALLEGRO_BITMAP*> parado[QTD_DIRECOES], 
+	FilaDupl<ALLEGRO_BITMAP*> andar[QTD_DIRECOES], 
+	FilaDupl<ALLEGRO_BITMAP*> atirar[QTD_DIRECOES], 
+	FilaDupl<ALLEGRO_BITMAP*> morrer[QTD_DIRECOES], ACAO nova_acao ) {
+
+	MudarDestinoOuAlvo(x_atual, y_atual, x_destino, y_destino, x_alvo, y_alvo);
+	MudarAcaoAtual(nova_acao);
+
+	for ( int i = 0; i < QTD_DIRECOES; i++ ) {
+		Parado[i] = parado[i]; //cria fulas duplas vazias
+		Andar[i] = andar[i]; //cria fulas duplas vazias
+		Atirar[i] = atirar[i]; //cria fulas duplas vazias
+		Morrer[i] = morrer[i]; //cria fulas duplas vazias
+	}
+}
+
+void SpriteManip::MudarDestinoOuAlvo( int x_atual, int y_atual, int x_destino,
+	int y_destino, int x_alvo, int y_alvo ) {
+
+	/*	SENTIDO HORÁRIO EM UM CÍRCULO
+		0: 345º - 15º: DIREITA
+		1: 15º - 75º: CIMADIR
+		2: 75º - 105º: CIMA
+		3: 105º - 165º: CIMAESQ
+		4: 165º - 195º: ESQUERDA
+		5: 195º - 255º: BAIXOESQ
+		6: 255º - 285º: BAIXO
+		7: 285º - 345º: BAIXODIR
+	*/
+	//seno de atual até destino (cateto oposto / hipotenusa)
+	double hipotenusa = sqrt( pow( x_destino - x_atual, 2 ) + pow( y_destino - y_atual, 2 ) );
+	double dest_sen = (y_destino - y_atual) / hipotenusa;
+	//cosseno de atual até destino (cateto adjacente / hipotenusa)
+	double dest_cos = (x_destino - x_atual) / hipotenusa;
+	//seno de atual até alvo (cateto oposto / hipotenusa)
+	double alvo_sen = (y_alvo - y_atual) / sqrt( pow( x_alvo - x_atual, 2 ) + pow( y_alvo - y_atual, 2 ) );
+	//cosseno de atual até alvo (cateto adjacente / hipotenusa)
+	double alvo_cos = (x_alvo - x_atual) / sqrt( pow( x_alvo - x_atual, 2 ) + pow( y_alvo - y_atual, 2 ) );
+
+	DIRECAO nova_dir_dest;
+	DIRECAO nova_dir_alvo;
+
+	//VERIFICANDO POSIÇÃO DESTINO
+	//cos > 0: semi cículo da direita
+	if ( dest_cos > 0 && dest_sen <= sin(15*PI/180) && dest_sen >= sin(345*PI/180) ) {
+		//DIREITA
+		nova_dir_dest = DIREITA;
+	}
+	else if ( dest_cos >= cos(75*PI/180) && dest_cos <= cos(15*PI/180) &&
+		dest_sen <= sin(75*PI/180) && dest_sen >= sin(15*PI/180) ) {
+		//CIMADIR
+		nova_dir_dest = CIMADIR;
+	}
+	//sen >0: semi círculo de cima
+	else if ( dest_sen > 0 && dest_cos <= cos(75*PI/180) && dest_cos >= cos(105*PI/180) ) {
+		//CIMA
+		nova_dir_dest = CIMA;
+	}
+	else if ( dest_cos >= cos(165*PI/180) && dest_cos <= cos(105*PI/180) &&
+		dest_sen >= sin(165*PI/180) && dest_sen <= sin(105*PI/180) ) {
+		//CIMAESQ
+		nova_dir_dest = CIMAESQ;
+	}
+	else if ( dest_cos < 0 && dest_sen <= sin(165*PI/180) && dest_sen >= sin(195*PI/180) ) {
+		//ESQUERDA
+		nova_dir_dest = ESQUERDA;
+	}
+	else if ( dest_cos >= cos(195*PI/180) && dest_cos <= cos(255*PI/180) && 
+		dest_sen >= sin(255*PI/180) && dest_sen <= sin(195*PI/180) ) {
+		//BAIXOESQ
+		nova_dir_dest = BAIXOESQ;
+	}
+	else if ( dest_sen < 0 && dest_cos >= cos(255*PI/180) && dest_cos <= cos(285*PI/180) ) {
+		//BAIXO
+		nova_dir_dest = BAIXO;
+	}
+	else {
+		//BAIXODIR
+		nova_dir_dest = BAIXODIR;
+	}
+
+	//VERIFICANDO POSIÇÃO ALVO
+	if ( alvo_cos > 0 && alvo_sen <= sin(15*PI/180) && alvo_sen >= sin(345*PI/180) ) {
+		//DIREITA
+		nova_dir_alvo = DIREITA;
+	}
+	else if ( alvo_cos >= cos(75*PI/180) && alvo_cos <= cos(15*PI/180) &&
+		alvo_sen <= sin(75*PI/180) && alvo_sen >= sin(15*PI/180) ) {
+		//CIMADIR
+		nova_dir_alvo = CIMADIR;
+	}
+	//sen >0: semi círculo de cima
+	else if ( alvo_sen > 0 && alvo_cos <= cos(75*PI/180) && alvo_cos >= cos(105*PI/180) ) {
+		//CIMA
+		nova_dir_alvo = CIMA;
+	}
+	else if ( alvo_cos >= cos(165*PI/180) && alvo_cos <= cos(105*PI/180) &&
+		alvo_sen >= sin(165*PI/180) && alvo_sen <= sin(105*PI/180) ) {
+		//CIMAESQ
+		nova_dir_alvo = CIMAESQ;
+	}
+	else if ( alvo_cos < 0 && alvo_sen <= sin(165*PI/180) && alvo_sen >= sin(195*PI/180) ) {
+		//ESQUERDA
+		nova_dir_alvo = ESQUERDA;
+	}
+	else if ( alvo_cos >= cos(195*PI/180) && alvo_cos <= cos(255*PI/180) && 
+		alvo_sen >= sin(255*PI/180) && alvo_sen <= sin(195*PI/180) ) {
+		//BAIXOESQ
+		nova_dir_alvo = BAIXOESQ;
+	}
+	else if ( alvo_sen < 0 && alvo_cos >= cos(255*PI/180) && alvo_cos <= cos(285*PI/180) ) {
+		//BAIXO
+		nova_dir_alvo = BAIXO;
+	}
+	else {
+		//BAIXODIR
+		nova_dir_alvo = BAIXODIR;
+	}
+
+	//se mudou de direção chama o primeiro sprite da ação atual
+	if ( nova_dir_alvo != DirecaoAlvo || nova_dir_dest != DirecaoDestino ) {
+		DirecaoAlvo = nova_dir_alvo;
+		DirecaoDestino = nova_dir_dest;
+		//desenhar teste
+		ALLEGRO_BITMAP *teste;
+		if ( AcaoAtual == PARADO ) {
+			Parado[DirecaoAlvo].GetFirstElem(teste);
+		}
+		else if ( AcaoAtual == ANDAR ) {
+			Andar[DirecaoAlvo].GetFirstElem(teste);
+		}
+		else if ( AcaoAtual == ATIRAR ) {
+			Atirar[DirecaoAlvo].GetFirstElem(teste);
+		}
+		else { //AcaoAtual == MORRER
+			Morrer[DirecaoAlvo].GetFirstElem(teste);
+		}
+		std::cout << teste << std::endl;
+	}
+	//senão avança o sprite da ação atual
+	else {
+		AvancarSprite();
+	}
+	//fazer verificação para ver se mudou alguma direção, se mudou a direção, chama novo sprite baseado na ação
+}
+
+void SpriteManip::MudarAcaoAtual( ACAO nova_acao ) {
+	if ( nova_acao != AcaoAtual ) {
+		AcaoAtual = nova_acao;
+	}
+}
+
+//avançar sprite não muda automaticamente quando a ação muda
+//avança sprite levando em conta alvo e destino, pode mudar o sentido de execução dependendo deles também
+void SpriteManip::AvancarSprite() {
+	ALLEGRO_BITMAP *teste;
+	if ( AcaoAtual == PARADO ) {
+		Parado[DirecaoAlvo].GetNextElem(teste);
+	}
+	else if ( AcaoAtual == ANDAR ) {
+		Andar[DirecaoAlvo].GetNextElem(teste);
+	}
+	else if ( AcaoAtual == ATIRAR ) {
+		Atirar[DirecaoAlvo].GetNextElem(teste);
+	}
+	else { //AcaoAtual == MORRER
+		Morrer[DirecaoAlvo].GetNextElem(teste);
+	}
+	std::cout << teste << std::endl;
+}
+
+SpriteManip::~SpriteManip() {
+
+}
---
+++ projeto-edb-lp/SpriteManip.h
@@ -0,0 +1,66 @@
+#ifndef _SPRITEMANIP_H_
+#define _SPRITEMANIP_H_
+	#include <allegro5/allegro.h>
+	#include <allegro5/allegro_image.h>
+	#include "ListaEncadeada/FilaDupl.h"
+	#include <cstddef>
+	#define QTD_DIRECOES 8
+	#define PI 3.1415926535897932
+
+namespace SpManip {
+	//CIMA = 0, CIMADIR = 1....
+	enum DIRECAO {CIMA = 0, CIMADIR, DIREITA, BAIXODIR,
+		BAIXO, BAIXOESQ, ESQUERDA, CIMAESQ};
+
+	enum ACAO { PARADO = 0, ANDAR, ATIRAR, MORRER };
+
+	class SpriteManip {
+		//vetor com ponteiros de imagens nas 8 direções
+	private:
+		DIRECAO DirecaoDestino; //aponta para onde está se movendo
+		DIRECAO DirecaoAlvo; //aponta para onde está mirando / olhando
+		ACAO AcaoAtual = PARADO;
+
+		//sprites que serão movidos quando objeto estiver parado
+		FilaDupl<ALLEGRO_BITMAP*> Parado[QTD_DIRECOES];
+		//sprites que serão movidos quando objeto estiver andando
+		FilaDupl<ALLEGRO_BITMAP*> Andar[QTD_DIRECOES];
+		//sprites que serão movidos quando objeto estiver atirando
+		FilaDupl<ALLEGRO_BITMAP*> Atirar[QTD_DIRECOES];
+		//sprites que serão movidos quando objeto estiver morrendo
+		FilaDupl<ALLEGRO_BITMAP*> Morrer[QTD_DIRECOES];
+	public:
+		//COSTRUTORES
+		//Construtor vazio
+		SpriteManip();
+		//construtores não recebem referência para não deletar ponteiros de onde foram chamados
+		//NECESSÁRIO DESTRUIR PONTEIROS DE ALLEGRO_BITMAP EM GM
+		//Construtor com Direções de destino e de alvo padrões
+		SpriteManip( FilaDupl<ALLEGRO_BITMAP*> parado[QTD_DIRECOES], 
+			FilaDupl<ALLEGRO_BITMAP*> andar[QTD_DIRECOES], 
+			FilaDupl<ALLEGRO_BITMAP*> atirar[QTD_DIRECOES], 
+			FilaDupl<ALLEGRO_BITMAP*> morrer[QTD_DIRECOES] );
+		//Construtor com Direções de destino e de alvo enviados
+		SpriteManip( int x_atual, int y_atual, int x_destino, int y_destino,
+			int x_alvo, int y_alvo,	FilaDupl<ALLEGRO_BITMAP*> parado[QTD_DIRECOES], 
+			FilaDupl<ALLEGRO_BITMAP*> andar[QTD_DIRECOES], 
+			FilaDupl<ALLEGRO_BITMAP*> atirar[QTD_DIRECOES], 
+			FilaDupl<ALLEGRO_BITMAP*> morrer[QTD_DIRECOES], ACAO nova_acao );
+		//DESTRUTOR
+		//destrutor não desaloca ponteiros de imagens
+		~SpriteManip();
+		//muda direção de destino
+		void MudarDestinoOuAlvo( int x_atual, int y_atual, int x_destino, int y_destino,
+			int x_alvo, int y_alvo );
+		//recalcular direcao de destino e alvo
+
+		//chamar mudar ação antes de mudar destino para mudar o sprite instantaneamente
+		void MudarAcaoAtual( ACAO nova_acao );
+
+		//ao chamar MudarDestinoOuAlvo(), não precisa chamar AvancarSprite()
+		//avança sprite levando em conta alvo e destino, pode mudar o sentido de execução dependendo deles também
+		void AvancarSprite();
+	};
+
+}
+#endif
