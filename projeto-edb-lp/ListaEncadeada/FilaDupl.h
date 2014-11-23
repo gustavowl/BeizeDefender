327780249af1a72959acd8a5049c9ce22716dcb4
Koruja <gustavoalvesbezerra@gmail.com>
2014-11-23 19:06:10 -0300

    Documenta Fila circular duplamente encadeada

--- projeto-edb-lp/ListaEncadeada/FilaDupl.h
+++ projeto-edb-lp/ListaEncadeada/FilaDupl.h
@@ -3,41 +3,94 @@
 	#include <iostream>
 	#include <cstddef>
 
-	//fila duplamente encadeada
-	//na realidade vai funcionar como uma mistura de fila e lista encadeada
-	//sempre insere no final da fila, remove do começo, mas o Get percorre um passo da lista
+	/**
+	* \class FilaDupl
+	*
+	* \brief Classe genérica de fila circula duplamente encadeada. Se comporta como uma mistura de fila e lista
+	* pois sempre insere no final da fila, remove do começo, mas pode-se obter qualquer elemento da fila
+	* pelo get.
+	*
+	* 
+	*
+	*/
 	template <class type>
 	class FilaDupl {
 	private:
-		int tam = 0;
+		int tam = 0; /**< Armazena o tamanho atual da fila*/
 
 	public:
 		struct node{
-			type val;
-			node* next = NULL;
-			node* prev = NULL;
+			type val; /**< Armazena o valor do nó atual*/
+			node* next = NULL; /**< Armazena o endereço de memória para o próximo nó. Por padrão é NULO*/
+			node* prev = NULL; /**< Armazena o endereço de memória para o nó anterior. Por padrão é NULO*/
 		};
-		node* first; //evita erro de apontar para 0x0 ou 0x1
-		//aponta originalmente pro último elemento.
-		//primeiro getnext retornará first. Primeiro getprev retornará o antepenúltimo
-		node* lastget; //último nó retornado pelo get
+		node* first; /**< Armazena o endereço de memória do primeiro nó da fila*/
+		node* lastget; /**< Armazena o endereço de memória do último nó retornado*/
 
+		/**
+		* Construtor padrão
+		* Inicializa tamanho, e ponteiros next e prev de first
+		*/
 		FilaDupl();
+		/**
+		* Construtor de cópia
+		* Faz cópia profunda da fila.
+		*/
 		FilaDupl(const FilaDupl &fila_dupl);
+		/**
+		* Destrutor
+		* Desoloca memória reservada dinamicamente pelos nós da fila
+		*/
 		~FilaDupl();
+		/**
+		* Sobrecarga do operador =
+		* Faz cópia profunda da fila.
+		*/
 		FilaDupl& operator=(const FilaDupl &fila_dupl);
+		/**
+		* Retorna o tamanho atual da lista
+		*/
 		int Size();
-		//bool GetElem(int pos, type &get) const;
+		/**
+		* Retorna o primeiro elemento da fila por referência
+		* \return Retorna se recebeu um valor válido por referência
+		* @param &get recebe o primeiro valor da fila por referência
+		*/
 		bool GetFirstElem(type &get);
+		/**
+		* Retorna o próximo elemento da fila por referência
+		* \return Retorna se recebeu um valor válido por referência
+		* @param &get recebe o próximo valor da fila por referência
+		*/
 		bool GetNextElem(type &get);
+		/**
+		* Retorna o elemento anterior da fila por referência
+		* \return Retorna se recebeu um valor válido por referência
+		* @param &get recebe o valor anterior da fila por referência
+		*/
 		bool GetPrevElem(type &get);
+		/**
+		* Retorna o último elemento retornado por referência
+		* \return Retorna se recebeu um valor válido por referência
+		* @param &get recebe o valor do último elemento retornado por referência
+		*/
 		bool GetLastGetElem(type &get);
-		//int Search(type val);
-		//bool Insert(int pos, type val);
+		/**
+		* Insere elemento na última posição da fila.
+		* \return Retorna se conseguiu inserir valor
+		* @param val valor a ser inserido na fila ou pilha
+		*/
 		bool Insert(type val);
-		//bool Remove(unsigned int pos);
+		/*
+		* Remove primeiro valor da fila (primeiro inserido)
+		* \return Retorna se removeu o elemento
+		*/
 		bool Remove();
-		//bool Remove(int pos, type &get);
+		/*
+		* Remove primeiro valor da fila (primeiro inserido) e retorna o valor removido por referência
+		* \return Retorna se removeu o elemento
+		* @param Recebe o valor removido por referência
+		*/
 		bool Remove(type &get);
 	};
 
