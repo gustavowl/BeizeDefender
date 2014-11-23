c8c12029b802e503653ab1d62f1ca512ab7c3a60
Koruja <gustavoalvesbezerra@gmail.com>
2014-11-23 18:42:31 -0300

    Documenta Lista encadeada

--- projeto-edb-lp/ListaEncadeada/lista.h
+++ projeto-edb-lp/ListaEncadeada/lista.h
@@ -3,266 +3,146 @@
 	#include <iostream>
 	#include <cstddef>
 
+	/**
+	* enum para o tipo de comportamento das listas
+	*/
 	enum BEHAVIOR {LISTASIMPLES, PILHA, FILA}; //0, 1, 2
 
+	/**
+	* \class Lista
+	*
+	* \brief Classe genérica de Lista encadeadas, pode se comportar como Lista simples, fila ou pilha.
+	*
+	* 
+	*
+	*/
 	template <class type>
 	class Lista {
 	private:
-		int tam = 0;
-		BEHAVIOR bhvr;
+		int tam = 0; /**< Armazena o tamanho atual da lista*/
+		BEHAVIOR bhvr; /**< Armazena o comportamento da lista: lista simples, pilha ou fila. Não pode ser alterado*/
 
 	public:
+		/**
+		* Estrutura de um nó da lista
+		*/
 		struct node{
-			type val;
-			node* next = NULL;
+			type val; /**< Armazena o valor do nó atual*/
+			node* next = NULL; /**< Armazena o endereço de memória para o próximo nó. Por padrão é NULO*/
 		};
-		node* head = new node; //evita erro de apontar para 0x0 ou 0x1
+		/**
+		* Nó cabeça. Assim que a classe é criada um endereço de memória é dinamicamente
+		* alocado pra ele, evitando que aponte para os endereços 0x0 e 0x1 vindos de lixo de memória.
+		*/
+		node* head = new node;
 
+		/**
+		* Construtor padrão
+		* Inicializa uma ListaSimples aponta o valor next de head para NULO
+		*/
 		Lista();
+		/**
+		* Construtor
+		* Inicializa uma Lista do tipo enviado por padrão.
+		* Aponta o valor next de head para NULO
+		* @param b tipo da lista a ser criada: simples, pilha ou fila
+		*/
 		Lista(BEHAVIOR b);
+		/**
+		* Construtor de cópia
+		* Faz cópia profunda da lista enviada por referência.
+		* @param &l recebe, por referência, lista a ser copiada
+		*/
 		Lista(const Lista &l);
+		/**
+		* Destrutor.
+		* Desaloca todos os nós da lista. Caso a lista seja de ponteiros, NÃO desaloca os ponteiros.
+		*/
 		~Lista();
+		/**
+		* Sobrecarga do operador =
+		* Faz cópia profunda da lista enviada por referência.
+		* @param &l recebe, por referência, lista a ser copiada
+		*/
 		Lista& operator=(const Lista &l);
+		/**
+		* Retorna o tamanho atual da lista
+		*/
 		int Size();
+		/**
+		* Retorna, por referência valor da posição enviada
+		* Funciona apenas para Listas com comportamento LISTASIMPLES, posições vão de 0 a tamanho - 1
+		* \return Retorna se conseguiu recuperar o valor da posição.
+		* @param pos Posição da onde será resgatado o valor
+		* @param &get receberá, por referência, valor da posição desejada
+		*/
 		bool GetElem(int pos, type &get) const;
+		/**
+		* Retorna, por referência próximo valor a ser retirado da pilha ou fila
+		* Funciona apenas para Listas com comportamento PILHA ou FILA
+		* \return Retorna se conseguiu recuperar o valor.
+		* @param &get receberá, por referência, próximo valor a ser retirado da pilha ou fila
+		*/
 		bool GetElem(type &get) const;
+		/**
+		* Procura por um valor na lista
+		* \return Retorna primeira instação do valor na lista. -1 caso não exista
+		* @param val contém valor a ser pesquisado
+		*/
 		int Search(type val);
+		/**
+		* Insere elemento na posição da lista.
+		* Funciona apenas para Listas com comportamento LISTASIMPLES, posições vão de 0 a tamanho - 1
+		* \return Retorna se conseguiu inserir valor na posição
+		* @param pos Posição onde será inserido o valor
+		* @param val valor a ser inserido na posição desejada
+		*/
 		bool Insert(int pos, type val);
+		/**
+		* Insere elemento na lista.
+		* Funciona apenas para Listas com comportamento FILA ou PILHA
+		* \return Retorna se conseguiu inserir valor
+		* @param val valor a ser inserido na fila ou pilha
+		*/
 		bool Insert(type val);
+		/*
+		* Remove valor de uma posição da lista
+		* Só funciona para listas simples. Posições vão de 0 a tamanho - 1
+		* \return Retorna se removeu o elemento da posição enviada
+		* @param pos posição da qual elemento será removido
+		*/
 		bool Remove(unsigned int pos);
+		/*
+		* Remove valor de uma posição da lista
+		* Só funciona para filas ou pilhas.
+		* \return Retorna se removeu o elemento
+		*/
 		bool Remove();
+		/*
+		* Remove valor de uma posição da lista, retornando-o por referência
+		* Só funciona para listas simples. Posições vão de 0 a tamanho - 1
+		* \return Retorna se removeu o elemento da posição enviada
+		* @param pos posição da qual elemento será removido
+		* @param &get recebe, por referência, valor removido da lista
+		*/
 		bool Remove(int pos, type &get);
+		/*
+		* Remove valor de uma posição da lista, retornando-o por referência
+		* Só funciona para filas ou pilhas.
+		* \return Retorna se removeu o elemento
+		* @param &get recebe, por referência, valor removido da fila/pilha
+		*/
 		bool Remove(type &get);
+		/*
+		* Imprime conteúdo da lista/fila/pilha
+		* @param separator caractere separador entre os valores, por padrão é o espaço
+		*/
 		void Print(char separator = ' ');
+		/*
+		* Retorna comportamento da lista
+		* \return Tipo de comportamento da lista
+		*/
 		BEHAVIOR GetBehavior() const;
-	//MÉTODOS
-		//construtores
-/*		Lista() {
-			bhvr = LISTASIMPLES;
-			//head = new node;
-			head->next = NULL;
-		}
-
-		Lista(BEHAVIOR b) {
-			bhvr = b;
-			//head = new node;
-			head->next = NULL;
-		}
-
-		Lista(const Lista &l) { //construtor de cópia (profunda)
-			bhvr = l.bhvr;
-			tam = l.tam;
-			node* temp = l.head->next;
-			node* copy = head;
-			while (temp != NULL) {
-				copy->next = new node;
-				copy = copy->next;
-				copy->val = temp->val;
-				temp = temp->next;
-			}
-		}
-		//destructor
-		~Lista() {
-			if (bhvr == LISTASIMPLES)
-				while (Remove(0));
-			else
-				while (Remove());
-			//delete head;
-		}
-
-		//operator =
-		Lista& operator=(const Lista &l) {
-			bhvr = l.bhvr;
-			tam = l.tam;
-			node* temp = l.head->next;
-			node* copy = head;
-			while (temp != NULL) {
-				copy->next = new node;
-				copy = copy->next;
-				copy->val = temp->val;
-				temp = temp->next;
-			}
-		}
-
-		//todos os métodos de com "int pos" funcionam como vetor (elementos de 0 até tamanho - 1)
-		int Size() {return tam;};
-
-		bool GetElem(int pos, type &get) { //retorna se posição é válida. &get armazenará valor
-			if (bhvr == LISTASIMPLES && pos >= 0 && pos < tam) {
-				node *temp = head;
-				for (int i = 0; i <= pos; i++) {
-					temp = temp->next;
-				}
-				get = temp->val;
-				return true;
-			}
-			return false;
-		};
-
-		bool GetElem(type &get) { //retorna se lista ou pilha possui elemento top. &get armazenará valor
-			if (tam > 0) {
-				if (bhvr == PILHA) {
-					get = head->next->val;
-					return true;
-				}
-				else if (bhvr == FILA) {
-					node* temp = head->next;
-					while (temp->next != NULL) {
-						temp = temp->next;
-					}
-					get = temp->val;
-					return true;
-
-				}
-				return false;
-			}
-			return false;
-		}
-
-		int Search(type val) { //retorna posição da primeira instância de val
-			//-1: não existe elemento
-			if (tam > 0) {
-				node *temp = head->next;
-				int i = 0;
-				do {
-					if (temp->val == val)
-						return i;
-					i++;
-					temp = temp->next;
-				} while (temp->next != NULL);
-				return -1;
-			}
-			return -1;
-		};
-		
-		bool Insert(int pos, type val) { //retorna se inserção OK. Insere val em lista simples
-			if (pos >= 0 && pos <= tam && bhvr == LISTASIMPLES) {
-				node *temp = head;
-				for (int i = 0; i < pos; i++) {
-					temp = temp->next;
-				}
-				node *novo = new node;
-				novo->val = val;
-				novo->next = temp->next;
-				temp->next = novo;
-				tam++;
-				return true;
-			}
-			return false;
-		}
-
-		bool Insert(type val) { //retorna se inserção OK. Insere val em filas e pilhas
-			if (tam >= 0 && (bhvr == PILHA || bhvr == FILA)) {
-				node *novo = new node;
-				while (novo == head) { //I HAVE NO IDEA OF WHY THIS HAPPENS
-					novo = new node;
-				}
-				novo->val = val;
-				novo->next = head->next;
-				head->next = novo;
-				tam++;
-				return true;
-			}
-			return false;
-		}
-
-		bool Remove(unsigned int pos) { //retorna se remoção OK. Não retorna valor. Funciona para listas
-			if (pos >= 0 && pos < tam && bhvr == LISTASIMPLES) {
-				node* ant = head;
-				node* temp = head->next;
-				for (int i = 0; i < pos; i++) {
-					temp = temp->next;
-					ant = ant->next;
-				}
-				ant->next = temp->next;
-				delete temp;
-				tam--;
-				return true;
-			}
-			return false;
-		};
-
-		bool Remove() {
-			if (tam > 0 && head->next != NULL) {
-				if (bhvr == PILHA) {
-					node *temp = head->next;
-					head->next = temp->next;
-					delete temp;
-					tam--;
-					return true;
-				}
-				else if (bhvr == FILA) {
-					node *bef = head; //before
-					node *fow = head->next; //foward
-					while (fow->next != NULL) {
-						fow = fow->next;
-						bef = bef->next;
-					}
-					delete fow;
-					bef->next = NULL;
-					tam--;
-					return true;
-				}
-				return false;
-			}
-			return false;
-		}
-
-		bool Remove(int pos, type &get) { //retorna se remoção OK. &get armazenará valor da posição
-			if (pos >= 0 && pos < tam && bhvr == LISTASIMPLES) {
-				node* ant = head;
-				node* temp = head->next;
-				for (int i = 0; i < pos; i++) {
-					temp = temp->next;
-					ant = ant->next;
-				}
-				ant->next = temp->next;
-				get = temp->val;
-				delete temp;
-				tam--;
-				return true;
-			}
-			return false;
-		};
-
-		bool Remove(type &get) { //se usar referência e type = int vai dar conflito com Remove(int pos)
-		//salvar imediatamente conteúdo de get em outra variável caso seja ponteiro
-			if (tam > 0) {
-				if (bhvr == PILHA) {
-					node *temp = head->next;
-					head->next = temp->next;
-					get = temp->val;
-					delete temp;
-					tam--;
-					return true;
-				}
-				else if (bhvr == FILA) {
-					node *bef = head; //before
-					node *fow = head->next; //foward
-					while (fow->next != NULL) {
-						fow = fow->next;
-						bef = bef->next;
-					}
-					get = fow->val;
-					delete fow;
-					bef->next = NULL;
-					tam--;
-					return true;
-				}
-				return false;
-			}
-			return false;
-		}
-
-		void Print(char separator = ' ') {
-			node *temp = head->next;
-			std::cout << std::endl;
-			while (temp != NULL) {
-				std::cout << temp->val << separator;
-				temp = temp->next;
-			}
-			std::cout << std::endl;
-		}*/
 	};
 
 	#include "lista.inl"
